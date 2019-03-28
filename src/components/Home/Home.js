2cdab7d835f5d27cf4bdacc315a504803ab5f3cc
Gabriel Grover <gabrielgrover@Gabriels-iMac.local>
2019-03-28 14:46:06 -0700

    update home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,16 +1,20 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 const Home = props => {
 
-  useEffect(() => {
-    props.login()
-  }, []);
-
   return (
-    <div>
-      hello world! your token is { props.token }
+    <div style={styles.container}>
+      hello world! your token is {props.token}
+      {props.children}
     </div>
   );
 };
+
+const styles = {
+  container: {
+    height: "100%",
+    width: "100%"
+  }
+}
 
 export default Home;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,14 +1,8 @@
 import { connect } from "react-redux";
 import Home from "./Home";
-import { login } from "../../actions";
 
 const mapStateToProps = ({ appData }) => ({
-  token: appData.sessionToken,
   errs: appData.authErrs
 });
 
-const mapDispatchToProps = {
-  login
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(Home);
+export default connect(mapStateToProps)(Home);
