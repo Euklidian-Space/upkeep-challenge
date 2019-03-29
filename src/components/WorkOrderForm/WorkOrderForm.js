41e8e9b02bd59cf3499156faee29f3321eb3ab14
Gabriel Grover <gabrielgrover@Gabriels-iMac.local>
2019-03-29 09:51:00 -0700

    implement workorder form

--- src/components/WorkOrderForm/WorkOrderForm.js
+++ src/components/WorkOrderForm/WorkOrderForm.js
@@ -1,11 +1,56 @@
 import React from "react";
+import { Form, Field } from "react-final-form";
+import "./styles.css";
 
 const WorkOrderForm = props => (
   <div>
-    <h1>Create a Work Order</h1>
-    <form onSubmit={props.onSubmit}>
-      {props.renderFields()}
-    </form>
+    <h1>Create Work Order</h1>
+    <Form
+      onSubmit={props.onSubmit}
+      render={({ handleSubmit, form, submitting, pristine }) => (
+        <form onSubmit={handleSubmit}>
+          <div>
+            <Field
+              name="title"
+              component="input"
+              type="text"
+              placeholder="Title"
+            />
+          </div>
+          <div>
+            <Field
+              name="description"
+              component="input"
+              type="text"
+              placeholder="Description"
+            />
+          </div>
+          <div>
+            <Field name="priority" component="select">
+              <option />
+              <option>None</option>
+              <option>Low</option>
+              <option>Medium</option>
+              <option>High</option>
+            </Field>
+          </div>
+          <div>
+            <Field
+              name="dueDate"
+              component="input"
+              type="date"
+            />
+          </div>
+          <button
+            type="button"
+            onClick={form.submit}
+            disabled={submitting || pristine}
+          >
+            Submit
+          </button>
+        </form>
+      )}
+    />
   </div>
 );
 
--- src/components/WorkOrderForm/index.js
+++ src/components/WorkOrderForm/index.js
@@ -1,56 +1,27 @@
-import React from "react";
-import { Form, Field } from "react-final-form";
-import "./styles.css";
+import { connect } from "react-redux";
+import { createWorkOrder } from "../../actions";
+import WorkOrderForm from "./WorkOrderForm";
 
-const onSubmit = values => alert(JSON.stringify(values));
+const getPriority = priority => ({
+  None: 0,
+  Low: 1,
+  Medium: 2,
+  High: 3
+}[priority]);
 
-const WrappedWorkOrderForm = () => (
-  <Form
-    onSubmit={onSubmit}
-    render={({ handleSubmit, form, submitting, pristine }) => (
-      <form onSubmit={handleSubmit}>
-        <div>
-          <Field
-            name="title"
-            component="input"
-            type="text"
-            placeholder="Title"
-          />
-        </div>
-        <div>
-          <Field
-            name="description"
-            component="input"
-            type="text"
-            placeholder="Description"
-          />
-        </div>
-        <div>
-          <Field name="priority" component="select">
-            <option />
-            <option>None</option>
-            <option>Low</option>
-            <option>Medium</option>
-            <option>High</option>
-          </Field>
-        </div>
-        <div>
-          <Field
-            name="dueDate"
-            component="input"
-            type="date"
-          />
-        </div>
-        <button
-          type="button"
-          onClick={form.reset}
-          disabled={submitting || pristine}
-        >
-          Submit
-        </button>
-      </form>
-    )}
-  />
-);
+const convertToMiliSeconds = dateString => Date.parse(dateString);
 
-export default WrappedWorkOrderForm;
+const mapStateToProps = ({ appData }) => ({
+  errs: appData.errs
+});
+
+const mapDispatchToProps = {
+  onSubmit: values => createWorkOrder({
+    title: values.title,
+    description: values.description,
+    priority: getPriority(values.priority),
+    dueDate: convertToMiliSeconds(values.dueDate)
+  })
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(WorkOrderForm);
