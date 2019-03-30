df9b05027a8755e0520d801f819d464951546862
Gabriel Grover <gabrielgrover@Gabriels-iMac.local>
2019-03-29 17:46:00 -0700

    update home styles

---
+++ src/components/Home/styles.js
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export default styled.div`
+  .home-container {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    width: 100vw;
+    height: 100vh;
+  }
+
+  .buttons {
+    display: flex;
+    flex-flow: row nowrap;
+    justify-content: center;
+    margin-top: 15px;
+  }
+`
