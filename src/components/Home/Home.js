b1d485caeb7f7281b4f0100e1efef4a5cab1209f
Gabriel Grover <gabrielgrover@Gabriels-iMac.local>
2019-03-29 17:45:22 -0700

    implement home screen

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,20 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import WorkOrders from "../WorkOrders";
+import WorkOrderForm from "../WorkOrderForm";
+import { PrimaryButton } from "../common";
 
-const Home = props => {
+const initialState = {
+  showWorkOrderForm: false,
+  showWorkOrders: false
+};
+
+const Home = () => {
+  const [state, setState] = useState(initialState);
 
   return (
-    <div style={styles.container}>
-      hello world! your token is {props.token}
-      {props.children}
+    <div className="home-container">
+      {state.showWorkOrders ?
+        <WorkOrders />
+        :
+        null
+      }
+      {
+        state.showWorkOrderForm ?
+          <div style={{ marginTop: 40 }}>
+            <WorkOrderForm />
+          </div>
+          :
+          null
+      }
+      <div className="buttons">
+        <PrimaryButton onClick={() => setState({ showWorkOrders: true })}>
+          Work Orders   
+        </PrimaryButton> 
+        <PrimaryButton onClick={() => setState({ showWorkOrderForm: true })}>
+          Create Work Order   
+        </PrimaryButton> 
+      </div>
     </div>
   );
 };
-
-const styles = {
-  container: {
-    height: "100%",
-    width: "100%"
-  }
-}
 
 export default Home;
