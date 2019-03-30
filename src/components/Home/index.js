503472a5a5aa475dc8ed640f968468c2d23eaf7d
Gabriel Grover <gabrielgrover@Gabriels-iMac.local>
2019-03-29 17:45:50 -0700

    implement home styled component

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,24 @@
+import React from "react";
 import { connect } from "react-redux";
+import { flow } from "lodash/fp";
 import Home from "./Home";
+import HomeStyles from "./styles";
 
 const mapStateToProps = ({ appData }) => ({
   errs: appData.authErrs
 });
 
-export default connect(mapStateToProps)(Home);
+const connectToRedux = connect(mapStateToProps);
+
+const applyStyle = StyledComponent => ComposedComponent => () => (
+  <StyledComponent>
+    <ComposedComponent />
+  </StyledComponent>
+);
+
+const connectAndStyleHome = flow(
+  connectToRedux,
+  applyStyle(HomeStyles)
+);
+
+export default connectAndStyleHome(Home);
